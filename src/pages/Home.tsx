5a7a375f12adc2ccd670d37c24e6b50b21a3c5eb
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-06-05 12:32:07 -0700

    First Commit for Vacation Package Shop

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        This is a store where you can shop for dream vacation destination
+        packages!
+        <br /> Please check out the store.
+      </p>
+    </>
+  )
+}
